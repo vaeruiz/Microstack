743d6f3149b65cbbda2cce5ac6c72d5dc2d77f35
Pedro Ángel (Bama) <60348762+vaeruiz@users.noreply.github.com>
2021-06-07 13:33:48 +0200

    Update microstack2.sh

--- microstack2.sh
+++ microstack2.sh
@@ -18,3 +18,14 @@ snap install microstack --devmode --edge
 
 # Declarar equipo como nodo de computo
 microstack init --auto --compute --join $tokenvar
+
+# Proceso de información acerca del inicio de sesión y cierre del script
+sleep 2s
+echo "Proceso de despliegue de nodo de cómputo acabado"
+echo "Mostranto credenciales de inicio de sesion en el servidor"
+sleep 2s
+clear
+echo "El usuario es:"
+echo "admin"
+echo "La contraseña es:"
+snap get microstack config.credentials.keystone-password
