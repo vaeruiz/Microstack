6f7dd26c39ac191b7a15a373da2f7c7f7a9948b4
Pedro Ángel (Bama) <60348762+vaeruiz@users.noreply.github.com>
2021-06-09 13:29:44 +0200

    Update microstack2.sh

--- microstack2.sh
+++ microstack2.sh
@@ -17,10 +17,10 @@ snap install microstack --devmode --edge
 microstack init --auto --compute --join $tokenvar
 
 # Proceso de información acerca del inicio de sesión y cierre del script
-sleep 2s
+echo -e
 echo "Proceso de despliegue de nodo de cómputo acabado"
 echo "Mostranto credenciales de inicio de sesion en el servidor"
-sleep 4s
+read -p "Pulse enter para mostrar las credenciales de inicio de sesión y finalizar la instalación"
 clear
 echo "El usuario es:"
 echo "admin"
