e08c2344022f276caf01df84cde66ec9c42cce37
Pedro Ángel (Bama) <60348762+vaeruiz@users.noreply.github.com>
2021-06-09 12:52:00 +0200

    Update microstack.sh

--- microstack.sh
+++ microstack.sh
@@ -4,14 +4,7 @@
 #set -x
 set -e
 
-# Variables para conexiones ssh
-direccion1=user@192.168.x.x
-direccion2=user2@192.168.x.x
-direccion3=user3@192.168.x.x
-homedir2=x
-homedir3=x
-sshpass2=x
-sshpass3=x
+
 
 # Instalar sshpass
 apt-get install sshpass -y
@@ -25,8 +18,8 @@ microstack init --auto --control
 # Obtener token para añadir nodo de computo
 microstack add-compute > token.txt
 
-# Enviar por ssh archivo con token y continuacion del script
-sshpass -p $sshpass2 scp token.txt $direccion2:$homedir2
-sshpass -p $sshpass2 scp Microstack/microstack2.sh $direccion2:$homedir2
-sshpass -p $sshpass3 scp token.txt $direccion3:$homedir3
-sshpass -p $sshpass3 scp Microstack/microstack2.sh $direccion3:$homedir3
+# Aviso de que ha terminado el despliegue y continuació
+echo -e
+echo "Se ha configurado la máquina. Realice conexiones ssh desde el nodo de control al resto de nodos para poder llevar a cabo el traspaso de archivos y ejecute filepass.sh"
+echo -e
+read -p "Presione enter para salir"
