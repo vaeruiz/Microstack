9e962441dab51d64f21813bc4a5232639db49940
Pedro Ángel (Bama) <60348762+vaeruiz@users.noreply.github.com>
2021-06-10 12:09:05 +0200

    Update microstack.sh

--- microstack.sh
+++ microstack.sh
@@ -15,14 +15,15 @@ snap install microstack --devmode --edge
 
 # Inicializar nodo de control
 microstack init --auto --control
-
+echo -e
+echo "La inicialización del nodo de control ha finalizado, creando token de acceso al cluster"
+sleep 3s
 # Obtener token para añadir nodo de computo
 microstack add-compute > token.txt
 sleep 3s
 # Aviso de que ha terminado el despliegue y continuació
 clear
-echo "Se ha configurado la máquina como nodo controaldor. Realice conexiones ssh desde el nodo de control al resto de nodos"
-echo "para poder llevar a cabo el traspaso de archivos."
+echo "Se ha configurado la máquina como nodo controaldor. Realice conexiones ssh desde el nodo de control al resto de nodos para poder llevar a cabo el traspaso de archivos."
 echo "A continuación, configure y ejecute filepass.sh."
 echo -e
 read -p "Presione enter para salir"
