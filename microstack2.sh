feed393a54e18a1f29f1a1ae151180ef67168dbf
Pedro Ángel (Bama) <60348762+vaeruiz@users.noreply.github.com>
2021-06-14 12:07:17 +0200

    Update microstack2.sh

--- microstack2.sh
+++ microstack2.sh
@@ -19,7 +19,7 @@ microstack init --auto --compute --join $tokenvar
 # Proceso de información acerca del inicio de sesión y cierre del script
 echo -e
 echo "Proceso de despliegue de nodo de cómputo acabado"
-echo "Mostranto credenciales de inicio de sesion en el servidor"
+echo "Recuperando credenciales de inicio de sesion en el servidor"
 read -p "Pulse enter para mostrar las credenciales de inicio de sesión y finalizar la instalación"
 clear
 echo "El usuario es:"
