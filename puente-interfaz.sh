e0697ad46d232040af9149858d1b64dfed8d4239
Pedro Ángel (Bama) <60348762+vaeruiz@users.noreply.github.com>
2021-06-15 14:14:12 +0200

    Create puente-interfaz.sh

---
+++ puente-interfaz.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables de entorno
+NOMBREINTERFAZ=
+IPINTERFAZ=/24
+# Crear alias de los servicos microstack
+sudo snap alias microstack.openstack openstack
+sudo snap alias microstack.ovs-vsctl ovs-vsctl
+
+# Puentear interfaz de red física con la interfaz virtual
+ovs-vsctl add-port br-ex $NOMBREINTERFAZ
+ip addr flush dev $NOMBREINTERFAZ
+ip addr add $IPINTERFAZ dev br-ex
+ip link set br-ex up
